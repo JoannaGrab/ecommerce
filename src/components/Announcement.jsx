2ef8835aba93dcb1655b2871b3fe027bd1e94684
JoannaGrab <huhu@mail.com>
2022-06-09 10:16:56 +0200

    add slider

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return <Container>Super Deal! Free Shipping on Orders Over $500</Container>;
+};
+
+export default Announcement;
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i5.walmartimages.ca/images/Enlarge/201/231/6000201201231.jpg",
+    title: "RACE BIKES",
+    desc: "THE WORLD'S FASTEST BIKES RIDDEN TO VICTORY IN GRAND TOURS.",
+  },
+  {
+    id: 2,
+    img: "https://prod-api.mediaexpert.pl/api/images/gallery/thumbnails/images/35/3586049/BOTTECCHIA-Gravel-Monster-M21-28-cali-meski-Zielony-mat-skos.jpg",
+    title: "AERO BIKES",
+    desc: "BENCHMARK AERODYNAMICS. WORLD RECORD SPEED. MAXIMUM INTEGRATION.",
+  },
+  {
+    id: 3,
+    img: "https://www.cannondale.com/-/media/cannondale/supersix-evo/ultegra-di2-launch/png/c21_c11301u_supersix_evo_crb_disc_ult_di2_mrc_3q.ashx",
+    title: "SUMMER SALE",
+    desc: "MODERN. DURABLE. VERSATILE.",
+  },
+];

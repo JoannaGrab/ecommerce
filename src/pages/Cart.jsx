3d2d01ba972f1092542e1c5288f7b16013c426e3
JoannaGrab <huhu@mail.com>
2022-06-13 13:14:48 +0200

    add cart page

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,240 @@
+import { Add, Remove } from "@material-ui/icons";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 60px;
+`;
+
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.div``;
+
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+  display: flex;
+  flex: 2;
+`;
+
+const Image = styled.img`
+  width: 300px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ProductName = styled.span``;
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+const PriceDetails = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  border: none;
+  height: 1px;
+  background-color: #eee;
+  margin: 20px 0px;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 35vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  font-weight: 600;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag (2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dw034b6ba4/images/full/full_2022_/2022/full_2022_endurace_cf-sl-7-disc-etap_3365_ye-bk_P5.jpg?sw=874&sfrm=png&q=90&bgcolor=F4F4F4" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Endurace CF SL 7 eTAP
+                  </ProductName>
+                  <ProductId>
+                    <b>ID: </b>92354828442914
+                  </ProductId>
+                  <ProductColor color="lemonchiffon	" />
+                  <ProductSize>
+                    <b>Size: </b> 2XS
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetails>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 3 840</ProductPrice>
+              </PriceDetails>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dwb9714acf/images/full/full_2022_/2022/full_2022_aeroad-cf-slx-7-disc-etap_3051_P08_P5.jpg?sw=874&sfrm=png&q=90&bgcolor=F4F4F4" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Aeroad CF SLX 7 Disc eTap
+                  </ProductName>
+                  <ProductId>
+                    <b>ID: </b>923548284429123
+                  </ProductId>
+                  <ProductColor color="cadetblue	" />
+                  <ProductSize>
+                    <b>Size: </b> M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetails>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>1</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 6 120</ProductPrice>
+              </PriceDetails>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 13 800</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 150</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -150</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 13 800</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
